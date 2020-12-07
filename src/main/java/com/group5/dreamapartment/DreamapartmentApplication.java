d5eade5ba3a38da7b2ae2fdc8d4ddb1f419999a5
Branchnew <branch@new.com>
2020-12-07 15:28:34 +0100

    test

---
+++ src/main/java/com/group5/dreamapartment/DreamapartmentApplication.java
@@ -0,0 +1,13 @@
+package com.group5.dreamapartment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamapartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamapartmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group5/dreamapartment/DreamapartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.dreamapartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamapartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
